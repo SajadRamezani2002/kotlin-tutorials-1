daa96ad9bfb986d19927c5a23a3164a7379b3e11
SajadRamezani2002 <92363087+SajadRamezani2002@users.noreply.github.com>
2024-05-10 19:19:29 +0330

    my kotlin Tutorials

---
+++ src/Start and Control characters.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("1-Hello World!")
+    println("2-Hello\n World!")
+    println("3-Hello\t World!")
+    println("4-Hello\r World!")
+    println("5-Hello\b World!")
+    println("6-Hello\' World!")
+    println("7-Hello\" World!")
+    println("8-Hello\\ World!")
+
+
+}
---
+++ src/Variable.kt
@@ -0,0 +1,16 @@
+fun main() {
+//    constant
+    val P : Double = 3.141592653589793238462643
+    println(P)
+//    Variable
+    var a : Boolean = true
+    println(a)
+    var b : Int = 1256
+    println(b)
+    var c : Float = 8.71841F
+    println(c)
+    var d : Byte = 127
+    println(d)
+    var e : Long = 555542
+    println(e)
+}
